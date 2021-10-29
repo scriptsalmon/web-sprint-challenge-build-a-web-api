1cc78ed069fd14dab0b543b179bb1ed9f0fe8848
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-10-29 10:50:40 -0700

    project get by id

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,21 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model');
+
+async function validateProjectId (req, res, next) {
+    try{
+        const validProject = await Projects.get(req.params.id);
+        if(validProject){
+            req.body = validProject;
+            next();
+        } else {
+            next({ status: 404, message: "no project with given id" })
+        }
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+module.exports = {
+    validateProjectId,
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,11 +1,25 @@
 // Write your "projects" router here!
 const express = require('express');
 const Projects = require('./projects-model');
+const { validateProjectId } = require('./projects-middleware');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({ message: 'whats up? projects router is!!' });
+    Projects.get()
+      .then(projects => {
+        res.status(200).json(projects);
+      })
+      .catch(err => {
+          res.status(400).json({ 
+            message: 'getting projects failed',
+            stack: err.stack
+            });
+      })
+})
+
+router.get('/:id', validateProjectId, (req, res) => {
+    res.status(200).json(req.body);
 })
 
 module.exports = router;
