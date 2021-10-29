87e6c56e52e4ed60d2278b5b2b452b314a258282
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-10-29 12:11:44 -0700

    actions router connected

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -6,5 +6,25 @@ const Actions = require('./actions-model');
 const router = express.Router();
 
 
+router.get('/', (req, res) => {
+    res.status(200).json({ message: "actions router synced"});
+})
+
+router.get('/:id', (req, res) => {
+    res.status(200).json('nice');
+})
+
+router.post('/', (req, res) => {
+    res.status(200).json('nice');
+})
+
+router.put('/:id', (req, res) => {
+    res.status(200).json('nice');
+})
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json('nice');
+})
+
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const actionsRouter = require('./actions/actions-router');
 // Build your projects router in /api/projects/projects-router.js
 const projectsRouter = require('./projects/projects-router');
 
-server.use('/api/users', actionsRouter);
+server.use('/api/actions', actionsRouter);
 server.use('/api/projects', projectsRouter);
 
 server.get('/', (req, res) => {
